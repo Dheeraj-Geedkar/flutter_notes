d3db1ebda497fea02a88c76945d4fdab57da5c1d
Dheeraj Geedkar <114930324+Dheeraj-Geedkar@users.noreply.github.com>
2025-11-26 20:16:05 +0000

    configuration config done

---
+++ flutter_codes/center/one.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text("Flutter widget"),
+        // title of display page app bar
+      ),
+      body:
+
+      /*   ex - 1
+
+      Center(
+          child:
+          Text("Hello World" , style: TextStyle( fontSize: 25),))
+        */
+
+      Center(
+        child: Container(
+
+          height: 100,
+          width: 200,
+          color: Colors.blueAccent,
+
+          // to make content inside container centrally alignned so wrap test inside center widget
+          child: Center(child: Text("This is centre of connatiner " , style: TextStyle( color: Colors.white ),)),
+        ),
+      )
+    );
+  }
+}
