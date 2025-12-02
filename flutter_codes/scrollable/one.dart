72fa29c062359e6f2cf0833af5499cc68ccb155b
Dheeraj Geedkar <114930324+Dheeraj-Geedkar@users.noreply.github.com>
2025-12-02 04:04:53 +0000

    configuration config done

---
+++ flutter_codes/scrollable/one.dart
@@ -0,0 +1,120 @@
+ body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+        
+            children : [
+        
+              SingleChildScrollView (
+
+                scrollDirection: Axis.horizontal ,
+
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right:11),
+                        height: 200,
+                        width : 200 ,
+                        color: Colors.deepOrange
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.amberAccent
+                      ) ,
+                  
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.blue
+                      ) ,
+                  
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.lightGreen
+                      ) ,
+                  
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.deepOrange
+                      ) ,
+                  
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.amberAccent
+                      ) ,
+                  
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.blue
+                      ) ,
+                  
+                      Container(
+                          margin: EdgeInsets.only(right:11),
+                          height: 200,
+                          width : 200 ,
+                          color: Colors.lightGreen
+                      )
+                    ],
+                  ),
+                ),
+              ) ,
+        
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.amberAccent
+              ) ,
+        
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.blue
+              ) ,
+        
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.lightGreen
+              ) ,
+
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.deepOrange
+              ) ,
+
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.amberAccent
+              ) ,
+
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.blue
+              ) ,
+
+              Container(
+                  margin: EdgeInsets.only(bottom:11),
+                  height: 300,
+                  color: Colors.lightGreen
+              )
+        
+            ]
+          ),
+        ),
+      )
